c8ec04ac285b04b4880417cf3ea11e1ce04526ef
Chance Harper <craftedgamz@outlook.com>
2025-05-23 19:19:07 -0600

    a

---
+++ random-messages.js
@@ -0,0 +1,26 @@
+const messages = [
+  "Built with Crafted care",
+  "Still unblockable. Still Crafted.",
+  "You found the good stuff",
+  "Im in your walls",
+  "Atlas is gay.",
+  "Fuck America, fuck you all!",
+  "Yeah chat im done.",
+  "Arsen 🔥🔥🔥",
+  "Chat GPT carrys this shi hard",
+  "God help me",
+  "Ok StudentVue, if you insist",
+  "Dont threaten me with a good time",
+  "Got-To-Go-Guardian",
+  "78",
+  "26",
+  "LXXVIII",
+  "1535 Colombia",
+  "Pulling up chat",
+  "Came with instrustions"
+];
+
+window.addEventListener("DOMContentLoaded", () => {
+  const message = messages[Math.floor(Math.random() * messages.length)];
+  document.getElementById("random-message").textContent = message;
+});
