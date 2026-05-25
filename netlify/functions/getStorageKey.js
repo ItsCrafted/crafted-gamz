778a605caae2e99c8dac9f7bf53aa626cef0197b
Crafted <banditkingharper@gmail.com>
2026-05-25 17:49:21 -0600

    rm

--- netlify/functions/getStorageKey.js
+++
@@ -1,8 +0,0 @@
-
-exports.handler = async function () {
-  const keyName = process.env.LOCALSTORAGE_KEY || 'loggedIn';
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ keyName }),
-  };
-};
