d1aaa9b781a1bf4a5c33b9d92f1bf7a768afad0d
Crafted <banditkingharper@gmail.com>
2026-06-04 17:41:38 -0600

    a

--- js/index.js
+++ js/index.js
@@ -40,6 +40,7 @@ const FIREBASE_CONFIG_URL = 'https://firebase.cdn.cgamz.online';
           const env = data.environment || 'website';
           const envMap = {
             desktop: 'desktop/index.html',
+            windowed: 'browser/index.html',
             browser:  'browser/index.html',
             website: 'site/index.html'
           };
